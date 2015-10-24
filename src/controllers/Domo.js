f317dcf6f9618dc369080f1617de5807811ffc17
JosephCoppola <jjc9718@rit.edu>
2015-10-24 18:50:48 -0400

    Added make logic for domo in Domo controller and hooked up Post request for /maker

--- src/controllers/Domo.js
+++ src/controllers/Domo.js
@@ -1,8 +1,35 @@
 var _ = require('underscore');
 var models = require('../models');
 
+var Domo = models.Domo;
+
 var makerPage = function(req,res){
 	res.render('app');
 };
 
+var makeDomo = function(req, res){
+	
+	if(!req.body.name || !req.body.age){
+		return res.status(400).json({error:"Both name and age are required"});
+	}
+	
+	var domoData = {
+		name: req.body.name,
+		age: req.body.age,
+		owner: req.session.account._id
+	};
+	
+	var newDomo = new Domo.DomoModel(domoData);
+	
+	newDomo.save(function(err) {
+		if(err){
+			console.log(err);
+			return res.status(400).json({error:"An error occured"});
+		}
+		
+		res.json({redirect: "/maker"});
+	});
+};
+
 module.exports.makerPage = makerPage;
+module.exports.make = makeDomo;
