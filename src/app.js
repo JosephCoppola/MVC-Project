f45b708115a02c7c7408291bcbba1f4ddfaa5441
JosephJCoppola <jjc9718@rit.edu>
2015-10-28 11:35:01 -0400

    Setting up Redis

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+var url = require('url');
 
 var dbURL = process.env.MONGOLAB_URI || "mongodb://localhost/DomoMaker";
 
@@ -16,6 +18,19 @@ var db = mongoose.connect(dbURL,function(err){
 		throw err;
 	}
 });
+
+var redisURL = {
+	hostname: 'localhost',
+	port: 6379	
+};
+
+var redisPASS;
+
+if(process.env.REDISCLOUD_URL)
+{
+	redisURL = url.parse(process.env.REDISCLOUD_URL);
+	redisPASS = redisURL.auth.split(":")[1];
+}
 
 var router = require('./router.js');
 
@@ -29,6 +44,11 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(session({
 		key: "sessionid",
+		store: new RedisStore({
+			host: redisURL.hostname,
+			port: redisURL.port,
+			pass: redisPASS
+		}),
 		secret: "Domo Arigato",
 		resave: true,
 		saveUninitialized: true
